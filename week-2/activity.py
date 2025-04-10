0d72f11cec8dab5d09b4977f57203f04c2d14c87
Caue Almeida <nncl@live.com>
2025-04-09 22:20:57 -0300

    feat: Practices random.seed method

---
+++ week-2/activity.py
@@ -0,0 +1,9 @@
+import random
+
+random.seed(3)
+print(random.randint(1, 1000))
+random.seed(3)
+print(random.randint(1, 1000))
+print(random.randint(1, 1000))
+
+# More in https://panda.ime.usp.br/pensepy/static/pensepy/04-Modulos/modulos.html
