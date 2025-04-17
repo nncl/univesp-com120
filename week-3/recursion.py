43e151fd81c92588062d985814adb3ced9f89e56
Caue Almeida <nncl@live.com>
2025-04-17 20:48:01 -0300

    refactor: Memoizes recursive function

--- week-3/recursion.py
+++ week-3/recursion.py
@@ -1,7 +1,13 @@
 def fat(n):
     return 1 if n == 0 else n * fat(n - 1)
 
+m = dict()
 def f(n):
     if n < 2: return n
 
-    return f(n - 1) + f(n - 2)
+    if m.get(n) is not None:
+        return m[n]
+
+    m[n] = f(n - 1) + f(n - 2)
+
+    return m[n]
