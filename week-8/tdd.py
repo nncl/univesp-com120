7724e627a4ca3989fa7e9b213bb6145b029f1c19
Caue Almeida <nncl@live.com>
2025-05-13 21:31:37 -0300

    test: Uses unitest instead default

--- week-8/tdd.py
+++ week-8/tdd.py
@@ -1,13 +1,12 @@
-from functions import sum
+import unittest
+from functions import sum, multiply
 
-def test_sum():
-    assert sum([1,2,3]) == 6, 'should return 6 when summing list'
+class Test(unittest.TestCase):
+    def test_sum(self):
+        self.assertEqual(sum([1,2,3]), 6, 'should return 6 when summing list')
 
-def test_multiply():
-    assert sum([1,2,3]) == 6, 'should return 6 when multiplying list'
+    def test_multiply(self):
+        self.assertEqual(multiply([1,2,3]), 6, 'should return 6 when multiplying list')
 
 if __name__ == '__main__':
-    test_sum()
-    test_multiply()
-
-    print('All good')
+    unittest.main()
